e4f923066232be287e3fee28797754805c3d2324
Samuel Bechara <sbechara@proton.me>
2022-04-18 11:18:18 -0600

    I added some more functionality to jog file

--- Field/jog.sh
+++ Field/jog.sh
@@ -2,9 +2,9 @@
 # THIS IS A VIRUS PROBABLY! JUST IGNORE EVERYTHING BELOW THIS
 currentDir=${PWD##*/} # removes pwd up to last slash and stores in var currentDir
 
-if [ $currentDir != 'Classroom']
+if [[ ! -n $(find study) || $currentDir != "Field" ]]
 then
-echo "You did not follow the instructions!"
+echo "You did not follow the instructions! Did you touch study? Are you in the correct folder?"
 else
 echo "Banana"
 fi
