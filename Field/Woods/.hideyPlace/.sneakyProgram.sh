7d49b641eef6774a1e6b7e90c675dae99e3f692c
Samuel Bechara <sbechara@proton.me>
2022-04-22 12:24:19 -0600

    slimmed up the find statement in sneakyProgram

--- Field/Woods/.hideyPlace/.sneakyProgram.sh
+++ Field/Woods/.hideyPlace/.sneakyProgram.sh
@@ -2,7 +2,7 @@
 if [ -f cheat ]
 then
 echo "You really should remove that cheat file before you run this program!"
-elif [[ -f think || -f think.txt ]]
+elif [[ -f think* ]]
 then
 echo "Nice work! You are almost done. You just have one more thing to do. Go checkout that door in the parent directory to this game and see if you can find anything you missed."
 else
